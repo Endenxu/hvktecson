0a9bdd518df4e82c6b865d0aba6b0157cb746e7d
Hugh <hughvenkyletecson@gmail.com>
2024-10-08 23:31:46 +0800

    Updates

--- src/components/projects/featuredData.js
+++ src/components/projects/featuredData.js
@@ -7,6 +7,14 @@ import P3 from '../../assets/project3.webp'
 export const FeaturedData = [
     {
         image: P1,
+        title: "Sdq",
+        demo: "https://play.google.com/store/apps/details?id=com.sdq.sdqapp&hl=en_US",
+        github: "https://github.com/Endenxu",
+        details: `Sdq Digital Signature Solution is a secure, enterprise-grade mobile application designed to streamline document workflows through advanced digital signature management. This powerful app enables businesses to create, verify, and manage digital signatures with robust encryption, ensuring document authenticity and integrity. Featuring intuitive document uploading, storage, and organization capabilities, along with customizable approval processes`,
+        tech: [ 'React Native', 'Expo', 'Typescript', 'Rest API']
+    },
+    {
+        image: P2,
         title: "Me App",
         demo: "https://github.com/maryvelez/AIFL-App",
         github: "https://github.com/maryvelez/AIFL-App",
@@ -14,20 +22,11 @@ export const FeaturedData = [
         tech: [ 'React Native', 'Tailwind', 'Expo']
     },
     {
-        image: P2,
-        title: "hvktAI - All in AI features",
-        demo: "https://github.com/Endenxu/hvktAI",
-        github: "https://github.com/Endenxu/hvktAI",
-        details: "This website is a hub for AI enthusiasts, offering a range of features powered by advanced technologies. Immerse yourself in the world of artificial intelligence with seamless integration of OpenAI API. Enjoy a smooth user experience on any device with our responsive HTML5 and CSS3 design. Built on Vercel, hvktAI ensures quick and reliable deployment for uninterrupted access to AI features.",
-        tech: [ 'JavaScript', 'CSS3', 'HTML5', 'OpenAI API', 'Vercel']
-    },
-    {
         image: P3,
-        title: "Portfolio Website",
+        title: "Ai Usage Webapp",
         demo: "https://hvktecson.vercel.app/",
         github: "https://github.com/Endenxu/hvktecson",
         details: "Discover the creative journey through my portfolio website. This platform showcases a collection of projects and insights, reflecting passion and proficiency in the digital realm. Explore a curated collection of projects presented through an interactive React-based interface. The website boasts a visually appealing design with HTML5 and CSS3, providing a seamless and engaging user experience. nHosted on Vercel, the portfolio website ensures fast and reliable performance.",
-        tech: [ 'React', 'HTML5', 'CSS3', 'Vercel']
+        tech: [ 'React', 'Node JS', 'Chrome Extension']
     },
 ];
-
